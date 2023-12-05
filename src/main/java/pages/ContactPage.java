0d2fdd6f3a13d321f194dbe2ccd8dd7c9c289be8
Kasper <104978402+k3anS@users.noreply.github.com>
2023-12-05 16:59:42 +0200

    Add Contact page

---
+++ src/main/java/pages/ContactPage.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static constants.XPath.ContactPage.*;
+
+public class ContactPage extends BasePage{
+    @FindBy(xpath = HEADER)
+    private WebElement contactHeader;
+    @FindBy(xpath = EMAIL_LABEL)
+    private WebElement emailLabel;
+    @FindBy(xpath = EMAIL_FIELD)
+    private WebElement emailField;
+    @FindBy(xpath = NAME_LABEL)
+    private WebElement nameLabel;
+    @FindBy(xpath = NAME_FIELD)
+    private WebElement usernameField;
+    @FindBy(xpath = MASSAGE_LABEL)
+    private WebElement massageLabel;
+    @FindBy(xpath = MASSAGE_FIELD)
+    private WebElement massageField;
+    @FindBy(xpath = CLOSE_BUTTON)
+    private WebElement closeButton;
+    @FindBy(xpath = SEND_BUTTON)
+    private WebElement sendButton;
+
+
+    public ContactPage(WebDriver driver) { super(driver); }
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    public void fillEmail(String email){
+        fillField(emailField, email);
+    }
+    public void fillUsername(String username){
+        fillField(usernameField, username);
+    }
+    public void fillMassage(String massage){fillField(massageField, massage); }
+    public String getEmailLabel(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(HEADER)));
+        return contactHeader.getText();
+    }
+
+}
