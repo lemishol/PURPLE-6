620a04bc12e4ad88e7867000677f26dd060f57b8
konoplytska <146989310+konoplytska@users.noreply.github.com>
2023-12-07 13:06:03 +0200

    Update Messages.java

--- src/main/java/constants/Messages.java
+++ src/main/java/constants/Messages.java
@@ -5,4 +5,8 @@ public class Messages {
          String REGULAR_MESSAGE = "Thank you for your purchase!",
                 ALERT_MESSAGE = "Please fill out Name and Creditcard.";
     }
+     public interface SignUpMessages{
+        String REGULAR_MESSAGE = "Thank you for your registration!",
+                ALERT_MESSAGE = "Please fill out Name and Creditcard.";
+    }
 }
