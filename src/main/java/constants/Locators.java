f8041e8f169f4c925662821b30c761e5dc6866df
KnightElmojr <114470285+KnightElmojr@users.noreply.github.com>
2023-12-07 19:57:03 +0200

    Delete src directory

--- src/main/java/constants/Locators.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class Locators {
-    public interface Item{
-        String FIRST_PRODUCT_XPATH = "//*[@class='card-title']//*[@href='prod.html?idp_=1']",
-        ADD_TO_CART_BUTTON_XPATH = "//*[contains(text(), 'Add to cart')]";
-    }
-}
