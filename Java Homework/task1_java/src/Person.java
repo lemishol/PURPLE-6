864e1e9bbbeb765a0122da49c2f8ef17c87809a2
Kasper <104978402+k3anS@users.noreply.github.com>
2023-12-08 14:31:05 +0200

    Update Person.java

--- Java Homework/task1_java/src/Person.java
+++ Java Homework/task1_java/src/Person.java
@@ -1,29 +1,42 @@
+
 import java.util.Scanner;
-public class person {
+import java.time.Year;
+
+public class Person {
     private String name;
     private int birthYear;
-    public person() {
+
+    // Default constructor  and constructor with 2 parameters
+    public Person() {
     }
-    public person(String name, int birthYear) {
+    public Person(String name, int birthYear) {
         this.name = name;
         this.birthYear = birthYear;
     }
+    // Getter and Setter for name
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
+    // Getter and Setter for birthYear
     public int getBirthYear() {
         return birthYear;
     }
+
     public void setBirthYear(int birthYear) {
         this.birthYear = birthYear;
     }
+
+    // Method to calculate age
     public int age() {
-        int currentYear = 2023;
+        int currentYear = Year.now().getValue();  // Assuming the current year is 2023
         return currentYear - birthYear;
     }
+
+    // Method to input information
     public void input() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter name: ");
@@ -31,12 +44,17 @@ public class person {
         System.out.print("Enter birth year: ");
         this.birthYear = scanner.nextInt();
     }
+
+    // Method to output information
     public void output() {
         System.out.println("Name: " + name);
         System.out.println("Birth Year: " + birthYear);
         System.out.println("Age: " + age());
     }
+
+    // Method to change the name
     public void changeName(String newName) {
         this.name = newName;
     }
 }
+
